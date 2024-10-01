dc3b4728d64c9f0cbd46335af8902856b900f9e0
kristapelayo <pelayokrista29@gmail.com>
2024-10-01 11:46:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Key({ label, onClick }) {
+  return (
+    <button className="Button" onClick={onClick}>
+      {label}
+    </button>
+  );
+}
+
+function Display({ value }) {
+  return (
+    <div className="Disp">
+      {value}
+    </div>
+  );
+}
+
 function App() {
+  const [displayValue, setDisplayValue] = useState("0");
+
+  const handleClick = (label) => {
+    setDisplayValue(prev => prev === "0" ? label.toString() : prev + label);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Calc">
+        <Display value={displayValue} />
+        <div className="ButtonsContainer">
+          <Key label={1} onClick={() => handleClick(1)} />
+          <Key label={2} onClick={() => handleClick(2)} />
+          <Key label={3} onClick={() => handleClick(3)} />
+          <Key label="+" onClick={() => handleClick('+')} />
+          <Key label={4} onClick={() => handleClick(4)} />
+          <Key label={5} onClick={() => handleClick(5)} />
+          <Key label={6} onClick={() => handleClick(6)} />
+          <Key label="-" onClick={() => handleClick('-')} />
+          <Key label={7} onClick={() => handleClick(7)} />
+          <Key label={8} onClick={() => handleClick(8)} />
+          <Key label={9} onClick={() => handleClick(9)} />
+          <Key label="x" onClick={() => handleClick('*')} />
+          <Key label={0} onClick={() => handleClick(0)} />
+          <Key label="C" onClick={() => setDisplayValue("0")} />
+          <Key label="=" onClick={() => {}} />
+          <Key label="/" onClick={() => handleClick('/')} />
+        </div>
+      </div>
     </div>
   );
 }
